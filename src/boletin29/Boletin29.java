ad1598426e3c6d192764680f3a7fcbbbb22bc8db
Juan Borrajo <juanborrajo10@gmail.com>
2018-05-13 11:20:23 +0200

    Clase main

---
+++ src/boletin29/Boletin29.java
@@ -0,0 +1,15 @@
+
+package boletin29;
+
+/**
+ *
+ * @author Juan Borrajo Rodriguez
+ */
+public class Boletin29 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
